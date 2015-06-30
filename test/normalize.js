530b8311db2cfaf0e1c4774e8eee56f9f3561082
Maciej Adwent <maciekadwent@gmail.com>
2015-06-30 09:30:54 -0700

    Update / cleanup of internal object representation

--- test/normalize.js
+++ test/normalize.js
@@ -1,4 +1,4 @@
-var Browsers = require("../src/browsers.js");
+var Browsers = require("../src/browsers");
 var Q = require("q");
 var fs = require("fs");
 var chai = require("chai");
@@ -27,22 +27,21 @@ describe("normalizer", function () {
     var id = "chrome_43_Windows_2012_R2_Desktop";
     var chrome43 = Browsers.get(id);
 
-    expect(chrome43).to.have.property("id", "chrome_43_Windows_2012_R2_Desktop");
+    expect(chrome43).to.have.property("browserName", "chrome");
     expect(chrome43).to.have.property("platform", "Windows 2012 R2");
-    expect(chrome43).to.have.property("family", "Chrome");
     expect(chrome43).to.have.property("version", "43");
     expect(chrome43).to.have.property("browserName", "chrome");
   });
 
   it("sets resolution correctly", function () {
     var chrome43 = Browsers.get("chrome_43_Windows_2012_R2_Desktop", "1024x768");
-    expect(chrome43).to.have.property("displayResolution", "1024x768");
+    expect(chrome43).to.have.property("screenResolution", "1024x768");
   });
 
   it("sets orientation correctly", function () {
     var id = "ipad_8_0_iOS_iPad_Simulator";
     var ipad = Browsers.get(id, undefined, "landscape");
-    expect(ipad).to.have.property("displayOrientation", "landscape");
+    expect(ipad).to.have.property("deviceOrientation", "landscape");
   });
 
 });
