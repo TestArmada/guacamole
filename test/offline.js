6c4d286c0d69f32c4ef9e89661c62f5a584d9a4b
Jack Herrington <jherrington@walmartlabs.com>
2017-01-09 11:06:41 -0800

    Migrating to ES6

--- test/offline.js
+++ test/offline.js
@@ -1,25 +1,23 @@
-/* eslint no-unused-vars: 0, no-undef: 0 */
+/* eslint no-unused-consts: 0, no-undef: 0 */
 "use strict";
-var Browsers = require("../src/browsers");
-var Q = require("q");
-var fs = require("fs");
-var chai = require("chai");
-var path = require("path");
+const Browsers = require("../src/browsers");
+const chai = require("chai");
+const path = require("path");
 
-var expect = chai.expect;
+const expect = chai.expect;
 
-describe("offline support", function () {
+describe("offline support", () => {
 
-  beforeEach(function () {
+  beforeEach(() => {
     Browsers.useShrinkwrap(path.resolve("./test_support/cached_sauce_api_result.json"));
   });
 
-  it("inflates a browser from a local shrinkwrap properly", function () {
-    var id = "chrome_43_Windows_2012_R2_Desktop";
-    var result = Browsers.get({ id: id });
+  it("inflates a browser from a local shrinkwrap properly", () => {
+    const id = "chrome_43_Windows_2012_R2_Desktop";
+    const result = Browsers.get({ id });
     expect(result).to.have.length(1);
 
-    var chrome43 = result[0];
+    const chrome43 = result[0];
     expect(chrome43).to.have.property("browserName", "chrome");
     expect(chrome43).to.have.property("platform", "Windows 2012 R2");
     expect(chrome43).to.have.property("version", "43");
